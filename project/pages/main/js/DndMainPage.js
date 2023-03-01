fc0bb364ad40c985e1377923ab539ac87a99717d
David Harrburger <dharrburger@outlook.com>
2023-03-01 13:55:30 +0100

    Update website structure - ground for comps

---
+++ project/pages/main/js/DndMainPage.js
@@ -0,0 +1,16 @@
+"use strict"
+
+import { DndApplicationBase } from "../../../js/DndApplicationBase";
+import { DndContactForm } from "../../../js/components/DndContactForm";
+
+class DndMainPage extends DndApplicationBase
+{
+    constructor()
+    {
+        super();
+        console.log("Hello DndMainPage");
+    }
+    
+}
+
+export { DndMainPage }
